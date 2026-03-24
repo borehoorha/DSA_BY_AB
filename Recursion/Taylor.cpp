c550cdd4c91403caead8db96fe6feed8d82ad3cb
DHRUV SONI <borehoorha@Lavikas-MacBook-Air.local>
2026-03-24 13:18:08 +0530

    Updated Taylor.cpp

--- Recursion/Taylor.cpp
+++ Recursion/Taylor.cpp
@@ -1,22 +1,39 @@
 #include<iostream>
 using namespace std;
 
-double e(int x,int n){
+double taylor_series_rec(int x,int n){
     static double p=1,f=1;
     double r;
     if(n==0){
         return 1;
     }
     else{
-        r=e(x,n-1);
+        r=taylor_series_rec(x,n-1);
         p=p*x;
         f=f*n;
         return r+p/f;
     }
 }
 
+double taylor_rec(double x, double n){
+    static double s = 1;
+    if(n==0)
+        return s;
+    s = x/n*s + 1;
+    return taylor_rec(x,n-1);
+}
+
+double taylor_series_loop(double x ,double n){
+    double s = 1;
+    for(;n>0;n--){
+        s = 1 + x/n*s;
+    }
+    return s;
+}
+
 int main(){
-    printf("%lf \n",e(1,10));     
-    cout<<e(1,10)<<endl;
+    cout<<taylor_series_rec(1,10)<<endl;
+    printf("%lf \n",taylor_series_loop(1,10));
+    cout<<taylor_rec(1,10)<<endl;     
     return 0;
 }
