aaa8c1962467d8dcf5ba0cf441403c19ad368105
DHRUV SONI <borehoorha@Lavikas-MacBook-Air.local>
2026-04-04 16:31:04 +0530

    Optimized Fibonacci.cpp

--- Recursion/Fibonacci.cpp
+++ Recursion/Fibonacci.cpp
@@ -20,8 +20,30 @@ int fib_rec(int n){
     return fib_rec(n-1) + fib_rec(n-2);
 }
 
+// OPTIMIZED RECURSION
+
+int F[10];
+int fib_opt_rec(int n){
+    if(n<=1){
+        F[n] = n;
+        return n;
+    }
+    else{
+        if(F[n-1]==-1)
+            F[n-1] = fib_opt_rec(n-1);
+        if(F[n-2]==-1)
+            F[n-2] = fib_opt_rec(n-2);
+        return F[n-1] + F[n-2];
+    }
+}
+
 int main(){
     cout<<fib_rec(5)<<endl;
     cout<<fib_loop(5)<<endl;
+
+    for(int i=0;i<10;i++)
+    F[i] = -1;
+
+    cout<<fib_opt_rec(5);
     return 0;
 } 
