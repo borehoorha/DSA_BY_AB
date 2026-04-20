19cf8f99d9fd9f15ac933693c73c12735825084c
DHRUV SONI <borehoorha@Lavikas-MacBook-Air.local>
2026-04-20 17:26:45 +0530

    UPDATED BASICS

--- ARRAY/1_Basics.cpp
+++ ARRAY/1_Basics.cpp
@@ -20,5 +20,51 @@ int main(){
     for(int i=0;i<5;i++){
         printf("%u : %d\n",&p[i],*(p+i));
     }
-    return 0;
+
+    // COPYING FROM ONE ARRAY TO ANOTHER
+    int *p = new int[5];
+    int *q = new int[10];
+
+    for(int i=0; i<5; i++)
+        p[i] = i+1;
+
+    for(int i=0; i<5; i++)
+        q[i] = p[i];
+
+    delete p;
+    p = q;
+    q = nullptr;
+
+    // 2-D ARRAYS
+
+    // METHODS TO INTIALIZE
+
+    int ARR[2][3];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            ARR[i][j] = j+i+1;
+    }
+
+    int *ptr_arr[2];
+    ptr_arr[0] = new int[3];
+    ptr_arr[1] = new int[3];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            ptr_arr[i][j] = j+i+1;
+    }
+    
+    int **p_arr;
+    p_arr = new int*[2];
+    p_arr[0] = new int[3];
+    p_arr[1] = new int[3];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            p_arr[i][j] = j+i+1;
+    }
+
+        return 0;
 }
